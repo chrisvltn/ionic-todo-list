b2daaa9fb4e431d777b5148dd18bbf657f2bbe5f
chrisvltn <chris_valentin@outlook.com>
2018-02-10 14:31:41 -0200

    Adds Edit/Create Task Page

--- src/pages/task-edit/task-edit.ts
+++ src/pages/task-edit/task-edit.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage } from 'ionic-angular';
+import { NavParams } from 'ionic-angular/navigation/nav-params';
+import { Task } from '../../models/Task';
+import { NavController } from 'ionic-angular/navigation/nav-controller';
 
 @IonicPage({
 	name: 'task-edit',
@@ -9,4 +12,27 @@ import { IonicPage } from 'ionic-angular';
 	selector: 'page-task-edit',
 	templateUrl: 'task-edit.html',
 })
-export class TaskEditPage { }
+export class TaskEditPage {
+	taskId: number
+	task: Task
+	edit: boolean
+
+	constructor(
+		params: NavParams,
+		private navCtrl: NavController,
+	) {
+		this.taskId = params.get('id')
+		this.edit = !!this.taskId
+		this.task = Task.parse()
+	}
+
+	async ngOnInit() {
+		if (!this.taskId) return;
+		this.task = await Task.findById(this.taskId)
+	}
+
+	async save() {
+		await this.task.save()
+		this.navCtrl.pop()
+	}
+}
